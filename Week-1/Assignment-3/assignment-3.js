705a730bbd346de95439e2c1b9de249dcaf41f2c
ferris110443 <ferris110443@hotmail.com.tw>
2024-01-19 03:10:22 +0800

    revise assign3 hw

--- Week-1/Assignment-3/assignment-3.js
+++ Week-1/Assignment-3/assignment-3.js
@@ -14,25 +14,7 @@ function countAandB(input) {
 function toNumber(input) {
 // your code here
     for(let i = 0; i < input.length ; i++){
-        switch (input[i]){
-            case 'a':
-                input[i] = 1;
-                break;
-            case 'b':
-                input[i] = 2;
-                break;
-            case 'c':
-                input[i] = 3;
-                break;
-            case 'd':
-                input[i] = 4;
-                break;
-            case 'e':
-                input[i] = 5;
-                break;
-            default:
-                console.log(`need add more case condition for index ${i}`)
-        }
+        input[i] = input[i].charCodeAt(0)-96;
     }
     return input;
 
@@ -45,3 +27,4 @@ console.log(toNumber(input1)); // should print [1, 2, 3, 1, 3, 1,3]
 let input2 = ['e', 'd', 'c', 'd', 'e'];
 console.log(countAandB(input2)); // should print 0
 console.log(toNumber(input2)); // should print [5, 4, 3, 4, 5]
+
